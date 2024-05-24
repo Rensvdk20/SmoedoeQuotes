32540a3282132bedcb672b22131c5df36ab8f70d
Rensvdk20 <rensvdk@gmail.com>
2024-05-24 17:04:33 +0200

    Added beginning of mongoose iplementation

--- quotes.js
+++ quotes.js
@@ -1,9 +1,21 @@
 import "dotenv/config";
 import axios from "axios";
+import mongoose from "mongoose";
 import FormData from "form-data";
 import cron from "node-cron";
 import * as fs from "fs";
 import { createCanvas, loadImage } from "canvas";
+
+const Schema = mongoose.Schema;
+const Quote = new Schema({
+	text: { type: String },
+	name: { type: String },
+	date: { type: Date, default: Date.now },
+});
+
+const conn = await mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
+
+const QuotesModel = conn.model("Quote", Quote);
 
 // Parse quote and extract the date
 function parseQuote(quote) {
@@ -77,6 +89,23 @@ async function generateImageWithQuote() {
 	}
 
 	const { name, quote, date } = quoteData;
+
+	//Check if quote is already used in the past 14 days
+	const isQuoteExists = await QuotesModel.exists({ text: quote });
+
+	if (isQuoteExists) {
+		console.log("Already exists");
+		console.log(quote);
+		return sendQuote();
+	} else {
+		console.log("Doesn't exist yet");
+		const qm = new QuotesModel({
+			quote,
+			name,
+		});
+
+		await qm.save(); // Save the new quote
+	}
 
 	// Set up the canvas with the background image
 	ctx.drawImage(await loadImage("https://picsum.photos/600/400"), 0, 0);
@@ -239,13 +268,13 @@ async function sendQuote() {
 			headers: data.getHeaders(),
 		};
 
-		await axios.request(config);
+		//? await axios.request(config);
 		console.log(`Send quote of the day (${getCurrentDate()})`);
 	} catch (error) {
 		console.error("Error sending quote", error);
 	}
 }
 
-cron.schedule("0 20 * * *", () => {
-	sendQuote();
-});
+//? cron.schedule("0 20 * * *", () => {
+sendQuote();
+//? });
